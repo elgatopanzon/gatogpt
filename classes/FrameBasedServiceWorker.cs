bb8e8028f11045d71114864fa0c2b8150c021c8e
ElGatoPanzon <gato@elgatopanzon.io>
2023-12-31 02:52:04 -0600

    FBSW: fix high CPU usage by using Task.Delay instead of an infinite while loop

--- classes/FrameBasedServiceWorker.cs
+++ classes/FrameBasedServiceWorker.cs
@@ -18,7 +18,7 @@ public partial class FrameBasedServiceWorker : BackgroundService
 	private double _frameCounter = 0;
 
 	// the time in seconds that we want to execute the task
-	private double _taskRunTimeoutSec = 0;
+	private double _taskRunTimeoutSec = 5;
 	private TimeSpan _taskRunCounter = new TimeSpan();
 
 	public FrameBasedServiceWorker()
@@ -38,8 +38,8 @@ public partial class FrameBasedServiceWorker : BackgroundService
 
         	if (_deltaTime.TotalMilliseconds >= (1 / _targetFps) * 1000)
         	{
-        		LoggerManager.LogDebug("Delta time", "", "deltaTime", _deltaTime);
-        		LoggerManager.LogDebug("Frame count", "", "frameCount", 1000 / _deltaTime.TotalMilliseconds);
+        		// LoggerManager.LogDebug("Delta time", "", "deltaTime", _deltaTime);
+        		// LoggerManager.LogDebug("Frame count", "", "frameCount", 1000 / _deltaTime.TotalMilliseconds);
         		_startTime = DateTime.Now;
 
         		_taskRunCounter += _deltaTime;
@@ -52,9 +52,19 @@ public partial class FrameBasedServiceWorker : BackgroundService
 				}
 
 				var _elapsedTime = DateTime.Now - _startTime;
-        		LoggerManager.LogDebug("Elapsed time", "", "elapsedTime", _elapsedTime.TotalMilliseconds);
+        		// LoggerManager.LogDebug("Elapsed time", "", "elapsedTime", _elapsedTime.TotalMilliseconds);
+
+        		// LoggerManager.LogDebug("Delta adjust", "", "deltaAdjust", _deltaTime - TimeSpan.FromMilliseconds((1 /_targetFps) * 1000));
+
+				var tilNextFrame = TimeSpan.FromMilliseconds((1 /_targetFps) * 1000) - _elapsedTime;
+        		// LoggerManager.LogDebug("Next frame in", "", "nextFrame", tilNextFrame - (_deltaTime - TimeSpan.FromMilliseconds((1 /_targetFps) * 1000)));
 
         		_frameCounter += 1 * _deltaTime.TotalMilliseconds / 1000;
+
+				if (tilNextFrame.TotalMilliseconds > 0)
+				{
+        			await Task.Delay(tilNextFrame, stoppingToken);
+				}
         	}
 
 			// set the end frame time
@@ -65,6 +75,7 @@ public partial class FrameBasedServiceWorker : BackgroundService
     private Task Run()
     {
         LoggerManager.LogDebug("Executing background service task");
+        LoggerManager.LogDebug("Frame count", "", "frameCount", 1000 / _deltaTime.TotalMilliseconds);
         System.Threading.Thread.Sleep(5);
 
         return Task.FromResult("Done");
